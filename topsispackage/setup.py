52cffa39188acbe89c01804d1ba99563f91574d2
KingParth904 <98462787+KingParth904@users.noreply.github.com>
2024-01-29 02:44:11 +0530

    Add files via upload

---
+++ topsispackage/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='topsis_102116006',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+)
